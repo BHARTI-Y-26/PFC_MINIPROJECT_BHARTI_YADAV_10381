90aee268b989a4b1029215eb470c7f5fdcf5a0d7
BHARTI-Y-26 <bharti260108@gmail.com>
2026-01-05 12:36:26 +0530

    Commit
    Added comments for project details and output examples.

--- password.c
+++ password.c
@@ -1,8 +1,11 @@
 #include <stdio.h>
-
+//Bharti Yadav
+//ERP-10381
+//Project title : Password Lock System
+//Topic Name : Login attempts
 int main()
 {
-    int password = 1234;   // fixed password
+    int password = 1234;   
     int input;
     int attempts = 3;
 
@@ -29,4 +32,12 @@ int main()
     }
 
     return 0;
+
 }
+//Output:
+//Enter password: 2345
+//❌ Wrong password. Attempts left: 2
+//Enter password: 5678
+//❌ Wrong password. Attempts left: 1
+//Enter password: 1234
+//✅ Access Granted! Login Successful.
